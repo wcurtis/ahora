1f82ea0032717e9fb420f8f2f87626cf863c1259
Bill Curtis <billcurt@gmail.com>
2012-12-03 01:06:30 -0500

    Added favicon

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ app.configure(function(){
   app.set('port', process.env.PORT || 5000);
   app.set('views', __dirname + '/app/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
