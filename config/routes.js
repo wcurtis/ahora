2381a42351a4cdea2a970179d4ffda87f91ba4b7
Bill Curtis <billcurt@gmail.com>
2012-12-08 14:12:18 -0500

    Changed page.js controller to hook.js

--- config/routes.js
+++ config/routes.js
@@ -8,9 +8,9 @@ module.exports = function (app) {
   app.get('/', index.index)
 
   // article routes
-  var page = require('../app/controllers/page')
-  app.get('/h/new', page.create)
-  app.get('/h/:id', page.show)
-  app.post('/', page.pingall)
-  app.post('/h/:id', page.ping)
+  var hook = require('../app/controllers/hook')
+  app.get('/h/new', hook.create)
+  app.get('/h/:id', hook.show)
+  app.post('/h/:id', hook.ping)
+  app.post('/', hook.pingall)
 }
