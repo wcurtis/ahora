479a34d45235a1645533fcbb7ad114a38d353b28
Bill Curtis <billcurt@gmail.com>
2012-12-09 18:28:35 -0500

    Added select/deselect on mouse over/leave of the page url

--- public/js/hook.js
+++ public/js/hook.js
@@ -14,6 +14,30 @@ $(document).ready(function () {
       }
   });
 
+  $("#url-well").mouseenter(function(){
+      selectText('url-well');
+    }).mouseleave(function(){
+      document.getSelection().removeAllRanges();
+    });
+
+function selectText(element) {
+    var doc = document
+        , text = doc.getElementById(element)
+        , range, selection
+    ;    
+    if (doc.body.createTextRange) { //ms
+        range = doc.body.createTextRange();
+        range.moveToElementText(text);
+        range.select();
+    } else if (window.getSelection) { //all others
+        selection = window.getSelection();        
+        range = doc.createRange();
+        range.selectNodeContents(text);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+}
+
   $('#btn-post').on('click', function() {
 
     console.log('button press');
